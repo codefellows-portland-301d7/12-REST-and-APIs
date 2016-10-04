b9265f236d7ef1e9bab069682720197746ec0edd
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2016-10-04 12:10:22 -0700

    init

---
+++ starter-code/scripts/models/repo.js
@@ -0,0 +1,18 @@
+(function(module) {
+  var reposObj = {};
+
+  reposObj.allRepos = [];
+
+  reposObj.requestRepos = function(callback) {
+    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+      Do not forget to call the callback! */
+  };
+
+  reposObj.withTheAttribute = function(myAttr) {
+    return reposObj.allRepos.filter(function(aRepo) {
+      return aRepo[myAttr];
+    });
+  };
+
+  module.reposObj = reposObj;
+})(window);
