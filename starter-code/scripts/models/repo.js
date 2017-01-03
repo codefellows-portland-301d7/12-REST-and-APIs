e9edf23d5084ecd8d50164f965e9b1a37d6c47f9
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-03 15:03:44 -0800

    worked through ajax call and compiler function

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,24 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax('https://api.github.com/user/repos', {
+      method: 'GET',
+      data: {
+        access_token: token
+      },
+      success: function(response) {
+        console.log('success');
+        console.log(response);
+        response.forEach(function(ele) {
+          reposObj.allRepos.push(ele);
+        });
+        callback();
+      },
+      error: function(response) {
+        console.log('error');
+        console.log(response);
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
