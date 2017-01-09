fcba8f33c0148d94676b9fa307f189612cb8d8e1
meghanroserebecca <meghanroserebecca@gmail.com>
2017-01-08 21:49:39 -0800

    fixed index and repoView.js and now code works!

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -8,16 +8,16 @@
     var template = Handlebars.compile(source);
     var html = template(ele);
     return html;
+  };
 
-    repoView.renderRepos = function() {
-      $('#about ul').empty().append(
-      reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
-      .map(repoCompiler)
+  repoView.renderRepos = function() {
+    $('#about ul').empty().append(
+    reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
+    .map(repoCompiler)
     );
-    };
+  };
     /* TODO: DONE// Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
-    reposObj.requestRepos(repoView.renderRepos);
-  };
-});
+  reposObj.requestRepos(repoView.renderRepos);
+})(window);
